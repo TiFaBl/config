ebef3e303828b030e362fd9b002461e48948689e
tfb <till.blesik@gmx.de>
2020-08-06 15:52:34 +0200

    DOOM emacs

--- install.sh
+++ install.sh
@@ -6,6 +6,15 @@ sudo apt install gcc make perl python3 r-base git curl wget zsh emacs docker.io 
 # cleanup
 sudo apt autoremove
 
+# DOOM emacs
+#https://github.com/hlissner/doom-emacs
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+#git
+git config --global user.email "till.blesik@gmx.de"
+git config --global user.name "tfb"
+
 # oh-my-zsh
 # https://www.howtoforge.com/tutorial/how-to-setup-zsh-and-oh-my-zsh-on-linux/
 #chsh -s $(which zsh)
