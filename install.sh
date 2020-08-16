8d2384107f1d3f12fcff40bc2089889f31349d51
tfb <till.blesik@gmx.de>
2020-08-16 14:29:30 +0200

    git ignores and i3

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # system basics
-sudo apt install gcc make perl python3 r-base git curl wget zsh emacs docker.io -y;
+sudo apt install i3 gcc make perl python3 r-base git curl wget zsh emacs shellcheck markdown ripgrep fd-find docker.io -y;
 
 # cleanup
 sudo apt autoremove
@@ -11,9 +11,15 @@ sudo apt autoremove
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 
+# Copy configs
+
+# Symlinks
+
+
 #git
 git config --global user.email "till.blesik@gmx.de"
 git config --global user.name "tfb"
+git config --global core.excludesfile ~/.gitignore_global
 
 # oh-my-zsh
 # https://www.howtoforge.com/tutorial/how-to-setup-zsh-and-oh-my-zsh-on-linux/
