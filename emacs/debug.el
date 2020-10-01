fdeca74a1635a162ac2419957a0cb9ed7970150e
tfb <till.blesik@gmx.de>
2020-10-01 09:48:49 +0200

    config to debug with

---
+++ emacs/debug.el
@@ -0,0 +1,19 @@
+(toggle-debug-on-error t)
+(load-file "/home/tfb/emacspeak/lisp/emacspeak-setup.el")
+
+(require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+;; ;; refresh manually to avoid waiting time at every startup
+;; (package-refresh-contents)
+
+;; (unless (package-installed-p 'use-package)
+;;   (package-install 'use-package))
+(require 'use-package)
+
+;; ;; EVIL
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
