96469d30bd0f33337874b4d28db900e9e43e40db
tfb <till.blesik@gmx.de>
2020-10-02 18:50:49 +0200

    moved back to dynamic keyword generation in org

--- emacs/init.el
+++ emacs/init.el
@@ -24,9 +24,9 @@
  '(elpy-modules
    '(elpy-module-company elpy-module-eldoc elpy-module-folding elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(gc-cons-threshold 20000000)
- '(org-hierarchical-todo-statistics nil t)
+ '(org-hierarchical-todo-statistics nil)
  '(package-selected-packages
-   '(ido evil-collection flx-ido smex helpful ido-completing-read+ all-the-icons doom-modeline projectile evil-org flycheck which-key magit solarized-theme elpy evil use-package))
+   '(flyspell-correct-ido flyspell-correct ido evil-collection flx-ido smex helpful ido-completing-read+ all-the-icons doom-modeline projectile evil-org flycheck which-key magit solarized-theme elpy evil use-package))
  '(sentence-end-double-space nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
